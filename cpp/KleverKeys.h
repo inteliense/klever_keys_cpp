81528bc8424c53d2ab9fd034cd5942202956d51f
Ryan Fitzgerald <101024833+inteliense@users.noreply.github.com>
2022-04-17 17:22:44 -0400

    Add files via upload

---
+++ cpp/KleverKeys.h
@@ -0,0 +1,18 @@
+//
+// Created by ryan on 10/28/21.
+//
+
+#ifndef KLEVERKEYS_H
+#define KLEVERKEYS_H
+
+#include <string>
+#include "HideString.h"
+
+class KleverKeys {
+
+public:
+    static std::string get(std::string index);
+};
+
+
+#endif //KLEVERKEYS_H
